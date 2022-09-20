0279516ef1f9e7994ea9dd6059b1b580d2c9292f
Kolotenko Danil <twich75687568@gmail.com>
2022-09-20 19:30:55 +0300

    bbb

--- src/main/java/com/efimchick/ifmo/io/filetree/FileTreeImpl.java
+++ src/main/java/com/efimchick/ifmo/io/filetree/FileTreeImpl.java
@@ -2,86 +2,100 @@ package com.efimchick.ifmo.io.filetree;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
+import java.util.*;
 
 public class FileTreeImpl implements FileTree {
 
     @Override
     public Optional<String> tree(Path path) {
         File file = new File(String.valueOf(path));
-        if ( !file.exists()) return Optional.empty();
-        if ( file.isFile()) {
+        if (!file.exists()) return Optional.empty();
+        if (file.isFile()) {
             return Optional.of(file.getName() + " " + file.length() + " bytes");
         }
         if (file.isDirectory()) {
 
-            return Optional.of(directoryTree(file, new ArrayList<>()));
+            return Optional.of(directoryTree(file, new ArrayList<>(), 0));
         }
         return Optional.empty();
     }
-    private String directoryTree(File folder, List<Boolean> lastFolders) {
-        String directory = "";
-        if (lastFolders.size() != 0)
-            directory += (!(lastFolders.get(lastFolders.size() -1 )) ? "├─ " : "└─ ");
-        directory += folder.getName() + " " + folderSize(folder);
+
+    private String directoryTree(File folder, List<Boolean> lastFolders, int level) {
+        StringBuilder directory = new StringBuilder();
+        if (lastFolders.size() != 0) {
+            for (int i = 0; i < level; i++) {
+                if (folder.isFile()) {
+                    directory.append("│  ");
+                }
+            }
+
+            directory.append(!(lastFolders.get(lastFolders.size() - 1)) ? "├─ " : "└─ ");
+        }
+
+        directory.append(folder.getName()).append(" ").append(folderSize(folder));
 
         File[] files = folder.listFiles();
+        assert files != null;
+
         int count = files.length;
         files = sortFiles(files);
+
+
         for (int i = 0; i < count; i++) {
-            directory += "\n";
+            directory.append("\n");
             for (Boolean lastFolder : lastFolders) {
                 if (lastFolder) {
-                    directory += "   ";
+                    directory.append("   ");
                 } else {
-                    directory += "│  ";
+                    directory.append("│  ");
                 }
             }
             if (files[i].isFile()) {
-                directory += (i + 1 == count ? "└" : "├") + "─ " +
-                        files[i].getName() + " " + files[i].length() + " bytes";
+                directory.append(i + 1 == count ? "└" : "├").append("─ ").append(files[i].getName()).append(" ").append(files[i].length()).append(" bytes");
             } else {
                 ArrayList<Boolean> list = new ArrayList<>(lastFolders);
-                list.add(i+1 == count);
-                directory += directoryTree(files[i], list);
+                list.add(i + 1 == count);
+                directory.append(directoryTree(files[i], list, level + 1));
             }
         }
-        return directory;
+        return directory.toString();
     }
+
     private long getFolderSize(File folder) {
         long size = 0;
         File[] files = folder.listFiles();
 
-        int count = files.length;
+        assert files != null;
 
-        for (int i = 0; i < count; i++) {
-            if (files[i].isFile()) {
-                size += files[i].length();
+        for (File file : files) {
+            if (file.isFile()) {
+                size += file.length();
             } else {
-                size += getFolderSize(files[i]);
+                size += getFolderSize(file);
             }
         }
         return size;
     }
+
     private String folderSize(File folder) {
         return getFolderSize(folder) + " bytes";
     }
-    private File[] sortFiles(File[] folder) {
 
-        Arrays.sort(folder);
-        List<File> sorted = new ArrayList<>();
+    private File[] sortFiles(File[] incomeData) {
 
-        for (int i = 0; i < folder.length; i++) {
-            if (folder[i].isDirectory()) sorted.add(folder[i]);
+        List<File> files = new ArrayList<>();
+
+        List<File> directories = new ArrayList<>();
+        for (File file : incomeData) {
+            if (file.isDirectory()) directories.add(file);
+            else files.add(file);
         }
 
-        for (int i = 0; i < folder.length; i++) {
-            if (folder[i].isFile()) sorted.add(folder[i]);
-        }
-        return sorted.toArray(new File[sorted.size()]);
+        directories.sort((o1, o2) -> o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase()));
+        files.sort((o1, o2) -> o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase()));
+
+        directories.addAll(files);
+
+        return directories.toArray(new File[directories.size()]);
     }
 }
